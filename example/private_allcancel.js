63c7dff52daf5044d09006165f1bc3cc96f920c3
you21979 <you2197901@gmail.com>
2014-11-19 21:41:08 +0000

    fix

---
+++ example/private_allcancel.js
@@ -0,0 +1,13 @@
+var RTH = require('..');
+var Constant = RTH.Constant;
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+var bot01 = config['bot01'];
+
+
+var rtj = RTH.createPrivateApi(bot01.address, bot01.secret, '');
+rtj.activeOrders('XRP_JPY').then(function(res){
+    return rtj.promise().all(res.map(function(v){return rtj.cancelOrder(v.id); }))
+}).then(console.log);
+
