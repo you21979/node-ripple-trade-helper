61574d324c1d112a60d650aa25ecc78a6d122ea3
you21979 <you2197901@gmail.com>
2014-11-22 12:10:56 +0000

    changemethod depth -> orderBook

--- example/depth.js
+++ example/depth.js
@@ -2,6 +2,6 @@ var RTH = require('..');
 var Constant = RTH.Constant;
 var api = RTH.createPublicApi(Constant.ISSUER.RIPPLE_TRADE_JAPAN);
 
-api.depth('XRP_JPY').then(console.log)
+api.orderBook('XRP_JPY').then(console.log)
 
 
