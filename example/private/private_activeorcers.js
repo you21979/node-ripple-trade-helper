200b380b780fdbfd595720b6e28fdb09f5565f39
you21979 <you2197901@gmail.com>
2014-11-23 21:39:24 +0000

    add support promise functions

---
+++ example/private/private_activeorcers.js
@@ -0,0 +1,11 @@
+var RTH = require('../..');
+var Constant = RTH.Constant;
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+var bot01 = config['bot01'];
+
+
+var rtj = RTH.createPrivateApi(bot01.address, bot01.secret, Constant.ISSUER.RIPPLE_TRADE_JAPAN);
+rtj.activeOrders('XRP_JPY').then(console.log);
+
