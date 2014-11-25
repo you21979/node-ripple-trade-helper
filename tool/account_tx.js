7b0dcd2a6a825c6db4ba3fbfe00ff3e3399bbb00
you21979 <you2197901@gmail.com>
2014-11-25 22:01:33 +0000

    fix print

--- tool/account_tx.js
+++ tool/account_tx.js
@@ -6,5 +6,8 @@ var api = RTH.createPublicApi();
 var opt = process.argv.splice(2)
 var address = opt.shift();
 
-api.accountTransactions(address).then(console.log).catch(console.log)
+api.accountTransactions(address).then(function(res){
+    console.log(res.account, res.ledger_index_max, res.ledger_index_min, res.limit, res.marker);
+    console.log(res.transactions.map(function(v){return v.tx}));
+}).catch(console.log)
 
