d10ac7801638b5023b03ceea540a763e6ac93b96
you21979 <you2197901@gmail.com>
2014-11-29 15:28:46 +0000

    delete example

--- example/tx.js
+++
@@ -1,7 +0,0 @@
-var RTH = require('..');
-var Constant = RTH.Constant;
-var api = RTH.createPublicApi(Constant.ISSUER.RIPPLE_TRADE_JAPAN);
-
-var txlist = [];
-api.transaction(txlist).then(console.log);
-
