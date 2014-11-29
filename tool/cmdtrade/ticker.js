24794b280f8f33b8ee1a8214a0672abb5a86bcac
you21979 <you2197901@gmail.com>
2014-11-29 19:47:54 +0000

    add tool

---
+++ tool/cmdtrade/ticker.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var RTH = require('../..');
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync("./wallet.json", "utf8"));
+
+var opt = process.argv.splice(2)
+if(opt.length !== 2){
+    console.log("%s exchange pair", process.argv[1])
+    console.log(" exchange = wallet name");
+    console.log(" pair = btc_xrp, xrp_jpy or otherpair");
+    return;
+}
+
+var exchange = config[opt.shift()];
+var pair = opt.shift();
+
+var dropdigit = function(value, digit){
+    var w = Math.pow(10, digit);
+    return Math.floor(value * w) / w;
+}
+var adjustPrice = function(price){ return dropdigit(price, 4) }
+var adjustAmount = function(amount){ return dropdigit(amount, 4) }
+var randomAmount = function(min, max){ return adjustAmount(min + (Math.random() * (max - min))) }
+
+var api = RTH.createPrivateApi(exchange.address, exchange.secret, exchange.issuer);
+api.orderBook(pair).then(function(res){
+    var ask = res.asks.splice(0, 1).map(function(v){return v[0]}).shift();
+    var bid = res.bids.splice(0, 1).map(function(v){return v[0]}).shift();
+    console.log("%s : %d - %d", pair, adjustPrice(bid), adjustPrice(ask))
+})
+
