b82c0bf44893e7dc7a52f22879680e3e2a0c1d23
you21979 <you2197901@gmail.com>
2014-11-29 15:55:38 +0000

    add tool

---
+++ tool/cmdtrade/balance.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var RTH = require('../..');
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync("./wallet.json", "utf8"));
+
+var opt = process.argv.splice(2)
+if(opt.length !== 1){
+    console.log("%s exchange address amount currency", process.argv[1])
+    console.log(" exchange = wallet name");
+    return;
+}
+
+var exchange = config[opt.shift()];
+
+var api = RTH.createPrivateApi(exchange.address, exchange.secret, exchange.issuer);
+api.balance().then(console.log).catch(console.log);
+
