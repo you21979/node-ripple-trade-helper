f0197a4fde146f890497269f6a48195d33ae68ae
you21979 <you2197901@gmail.com>
2014-12-01 15:56:36 +0000

    add stream connect

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var PublicApi = require('./lib/public_api');
+var PublicStream = require('./lib/public_stream');
 var PrivateApi = require('./lib/private_api');
+var PrivateStream = require('./lib/private_stream');
 var util = require('util');
 
 exports.Constant = require('./lib/constant');
 exports.createPublicApi = function(issuer){ return new PublicApi(issuer); }
+exports.createPublicStream = function(issuer){ return new PublicStream(issuer); }
 exports.createPrivateApi = PrivateApi;
+exports.createPrivateStream = PrivateStream;
 exports.util = require('./lib/ripple_util');
 exports.console = function(v){ console.log(util.inspect(v, { showHidden: true, depth: null })) }
