a24eaa646f9f68cd8333de577f3890933fa155c1
you21979 <you2197901@gmail.com>
2014-12-01 20:31:21 +0000

    mod

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var PublicApi = require('./lib/public_api');
 var PublicStream = require('./lib/public_stream');
 var PrivateApi = require('./lib/private_api');
 var PrivateStream = require('./lib/private_stream');
+var Promise = require('bluebird');
 var util = require('util');
 
+exports.promise = function(){ return Promise }
 exports.Constant = require('./lib/constant');
 exports.createPublicApi = function(issuer){ return new PublicApi(issuer); }
 exports.createPublicStream = function(issuer, callbacks){ return new PublicStream(issuer, callbacks); }
