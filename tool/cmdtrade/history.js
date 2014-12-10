bcacd7d8f8e8112c14f7537c8f76170d531bdc50
you21979 <you2197901@gmail.com>
2014-12-10 08:50:44 +0000

    mod

--- tool/cmdtrade/history.js
+++ tool/cmdtrade/history.js
@@ -8,12 +8,25 @@ var opt = process.argv.splice(2)
 if(opt.length !== 1){
     console.log("%s exchange orderid", process.argv[1])
     console.log(" exchange = wallet name");
-    console.log(" orderid = number");
     return;
 }
 
 var exchange = config[opt.shift()];
 
 var api = RTH.createPrivateApi(exchange.address, exchange.secret, exchange.issuer);
-api.history().then(console.log).catch(console.log);
+api.history().then(function(res){
+
+    res.forEach(function(v){
+        var w = [
+            v.orderid,
+            v.state,
+            v.pair,
+            v.trade,
+            v.amount[0],
+            v.amount[1]
+        ]
+        console.log(w.join(','))
+    })
+
+}).catch(console.log);
 
