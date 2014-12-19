c65665fd83dfada250ce4f7b10363531d095899b
you21979 <you2197901@gmail.com>
2014-12-19 12:00:15 +0900

    add trust

---
+++ tool/cmdtrade/trust.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+var RTH = require('../..');
+
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync("./wallet.json", "utf8"));
+
+var opt = process.argv.splice(2)
+if(opt.length !== 2){
+    console.log("%s exchange pair type price amount", process.argv[1])
+    console.log(" exchange = wallet name");
+    console.log(" trust IOU = IOU name");
+    return;
+}
+
+var exchange = config[opt.shift()];
+var currency = opt.shift();
+
+var api = RTH.createPrivateApi(exchange.address, exchange.secret, exchange.issuer);
+api.trust(currency.toUpperCase(), 1000000);
+
